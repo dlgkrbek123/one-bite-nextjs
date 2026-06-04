25a58402b0ae17bbc62dc97ab87d147e3a755095
Hakyu Lee <dlgkrbek123@gmail.com>
2026-06-04 16:33:43 +0900

    페이지 라우팅 설정 완료

---
+++ 2_page_router/section02/src/pages/404.tsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <>
+      <div>못 찾은게 아리사의 잘못이더냐?</div>
+    </>
+  );
+}
---
+++ 2_page_router/section02/src/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <>
+      <h1>제하하하하하</h1>
+    </>
+  );
+}
