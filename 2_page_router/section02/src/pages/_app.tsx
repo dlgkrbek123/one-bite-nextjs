478a26b2ab96e8c5ea5c0a0381ca6c99e423b14b
Hakyu Lee <dlgkrbek123@gmail.com>
2026-06-06 16:57:20 +0900

    페이지 라우터 내비게이션

--- 2_page_router/section02/src/pages/_app.tsx
+++ 2_page_router/section02/src/pages/_app.tsx
@@ -1,6 +1,26 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push("/test");
+  };
+
+  return (
+    <>
+      <header>
+        <Link href="/">index</Link>&nbsp;
+        <Link href="/search">search</Link>&nbsp;
+        <Link href="/book/1">book</Link>
+        <div>
+          <button onClick={handleClick}>/test 페이지로 이동</button>
+        </div>
+      </header>
+      <Component {...pageProps} />
+    </>
+  );
 }
